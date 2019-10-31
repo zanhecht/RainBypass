e0c6b14ea67ebf56ac26509fd96ddec3253973d9
zanhecht <ahecht@earthlink.net>
2019-10-31 17:14:38 -0400

    Update rain-bypass.py

--- rain-bypass.py
+++ rain-bypass.py
@@ -1,177 +1,191 @@
+## Based on rain-bypass.py from https://www.thirdeyevis.com/pi-page-3.php
+
 import urllib.request
-import urllib.parse
-import datetime
-import json
-import time
+import json 
 import RPi.GPIO as GPIO ##Import GPIO library
 import os
+import time
+try:
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
 
 ## Setup GPIO I/O PIns to output mode
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(7, GPIO.OUT) ## This pin controls relay switch. When ON/True, watering is disabled. Default OFF
-GPIO.setup(11, GPIO.OUT) ## This pin controls a flashing red light that flashes when data error
+GPIO.setup(7, GPIO.OUT) ## This pin controls relay switch.
+                        ## When ON/True, watering is disabled. Default OFF
+GPIO.setup(11, GPIO.OUT) ## This pin controls a flashing red light that flashes
+                        ##when data error
 GPIO.setup(13, GPIO.OUT) ## This pin enables green light when watering
 GPIO.setup(15, GPIO.OUT) ## This pin enables red light when watering disabled
 
-wundergroundKey = "xxxxxxxxxxxxxxxx" ## Enter your Wunderground key acquired by joining weather underground api program
-lastRain = 0 ## Hold epoch of last rain - float
-checkIncrement = 0  ## Amount of time between weather.com forecast requests - integer
-daysDisabled = 0 ## Days to disable systems prior to and after rain - integer
-zipCode = 0 ## Zip code for weather request - integer
-rainForecasted = False ## Is rain forecasted within daysDisabled forecast range - Boolean, global
+historicalRain = [] ## Hold historical rain forecasts
+config = {}         ## Hold configuration
 
-## Define conditions that will disable watering.  This includes Light/Heavy prefix on any of the following conditions
-## Weather.com Condition Phrases: http://www.wunderground.com/weather/api/d/docs?d=resources/phrase-glossary
-possibleConditions = ["Rain",
-                      "Rain Showers",
-                      "Thunderstorm",
-                      "Thunderstorms and Rain",
-                      ]
+## This funtion gets the path of this file.  When run at startup, we need full
+## path to access config file.
+## To run automatically at startup, change permission of this file to execute
+## If using wireless for network adapter, make sure wireless settings are
+## configured correctly in wlan config so wifi device is available on startup
+## edit /etc/rc.local file 'sudo pico /etc/rc.local'
+## add "python /home/pi/working-python/weather-json.py &" before line "exit 0"
 
-## Prefix each possible condition with 'Heavy' and 'Light', since these are possible conditions
-for x in possibleConditions[:]: ## Use slice notation to iterate loop inside slice copy of list
-    possibleConditions.insert(0,'Light ' + x)
-    possibleConditions.insert(0,'Heavy ' + x)
-
-##This funtion gets the path of this file.  When run at startup, we need full path to access config file
-##To run this file automatically at startup, change permission of this file to execute
-##If using wireless for network adapter, make sure wireless settings are configured correctly in wlan config so wifi device is available on startup
-##edit /etc/rc.local file 'sudo pico /etc/rc.local'
-##add "python /home/pi/working-python/weather-json.py &" before line "exit 0"
 def GetProgramDir():
    try:  ## If running from command line __file__ path is defined
       return os.path.dirname(os.path.abspath(__file__)) + "/"
-   except:  ## If __file__ is undefined, we are running from idle ide, which doesn't use this var
+   except:  ## If __file__ is undefined, we are running from idle ide
       return os.getcwd() + "/"
 
 ## Load values from config file, or create it and get values
 try: ## see if config file exists
-    configFile = open(GetProgramDir() + "rain-bypass.cfg","r")  ## Attempt to open existing cfg file
+    configFile = open(GetProgramDir() + "rain-bypass.cfg","r")
     print("Config file found, loading previous values...")
-    zipCode = int(configFile.readline()) ## Convert zip to int to remove unicode formatting, store in zipCode
-    daysDisabled = int(configFile.readline()) ## Convert second line to int and store in daysDisabled var
-    checkIncrement = int(configFile.readline()) ## Conver third line to int and store in checkIncrement var
+    config = json.load(configFile)
+    config["latValue"] = float(config["latValue"]) 
+    config["longValue"] = float(config["longValue"]) 
+    config["lookAhead"] = min(int(config["lookAhead"]),168)
+    config["lookBehind"] = min(int(config["lookBehind"]) ,168)
+    config["rainfallLimit"] = float(config["rainfallLimit"])
+    config["checkIncrement"] = int(config["checkIncrement"])
+    print("Finished loading previous values.")
     configFile.close()
 except: ## Exception: config file does not exist, create new
-    print("Config file not found, creating new...")
+    print("Config file not found, or values invalid. Creating new...")
 
-    ## Request zip code for request
-    latValue = int(input("Enter Latitude (##.#### or -##.####): "))
-    longValue = int(input("Enter Longitude (##.#### or -##.####): "))
-    latLong = str(latValue) + "," + str(longValue)
+    ## Request coordinates for request
+    config["latValue"] = input("Enter Latitude (##.#### or -##.####): ")
+    config["longValue"] = input("Enter Longitude (##.#### or -##.####): ")
 
-    ## input number of days system will be disabled prior to rain, and after rain
-    daysDisabled = int(raw_input("Enter number of days to disable system prior/after rain (between 1 and 9): "))
+    ## input number of hours to check for rain before and after current time
+    config["lookAhead"] = min(int(input("Enter number of hours to look ahead for rain (1 to 168): ")),168)
+    config["lookBehind"] = min(int(input("Enter number of hours to look back for rain (1 to 168): ")),168)
+
+    ## input rainfall limit
+    config["rainfallLimit"] = float(input("Enter rainfall amount that will disable watering, in inches/week: "))
 
     ## request number of checks in 24 hour period
-    checkIncrement = int(raw_input("Enter number of times you want to check forecast per 24-hour period (no more than 500, try 24, or once per hour): "))
-    checkIncrement = 86400/checkIncrement ## This is the wait interval between each check in seconds
+    ## checkIncrement = int(input("Enter number of times you want to check forecast per 24-hour period " + \
+    ##                           "(no more than 500, try 24, or once per hour): "))
+    checkIncrement = 24 ## Must check once per hour for lookback feature to work
+    config["checkIncrement"] = int(86400/checkIncrement) ## This is the wait interval between each check in seconds
     
     ## Save user input to new config file
     configFile = open(GetProgramDir() + "rain-bypass.cfg","w")
-    configFile.write(str(zipCode) + "\n" + str(daysDisabled) + "\n" + str(checkIncrement) + "\n") ## Write each item to new line
+    json.dump(config, configFile)
     configFile.close()
 
 ## Show values/interval used to check weatherc
-print("Checking forecast for point: " + str(latLong))
-print("System will be disabled for " + str(daysDisabled) + " days prior to and after rain")
-print("System will wait " + str(checkIncrement) + " seconds between checks")
-print("     or " + str(float(checkIncrement) / 60) + " minute(s) between checks")
-print("     or " + str(float(checkIncrement) / 3600) + " hour(s) between checks")
+print("Checking forecast for point: " + str(config["latValue"]) + ", " + \
+      str(config["longValue"]))
+print("System will look for rain " + str(config["lookAhead"]) +  " hours ahead and " + \
+       str(config["lookBehind"]) + " hours behind the current time.")
+print("System will be disabled if rainfall rate over that period is more than \n     " + \
+      str(config["rainfallLimit"]) + " inches per week.")
+print("System will wait " + str(config["checkIncrement"]) + " seconds (" + \
+      str(float(config["checkIncrement"]) / 60) + " minute(s) or " + \
+      str(float(config["checkIncrement"]) / 3600) + " hour(s)) between checks.")
 
 def CheckWeather():
-
-    ## This function will modify the following variables in the main scope
-    global rainForecasted
-    global lastRain
+    ## Create array for cached Quantitative Precipitation Forecast values
+    oldQpf = [] 
     
     while True: ## Loop this forever
+        rainForecasted = False ## Does rain exceed limit - Boolean
+        
         try:
-            ##Request Weather Data
-            request = "https://api.weather.gov/points/" + urllib.parse.quote(latLong)
-            response = urllib.request.urlopen(request)
-            jsonData = json.load(response)
-    
-            #request = jsonData["properties"]["forecast"]
-            request = jsonData["properties"]["forecastGridData"]
-            response = urllib.request.urlopen(request)
-            jsonData = json.load(response)
-            #Compare timestamp with datetime.datetime.fromisoformat("2019-05-09T23:07:32+00:00") - datetime.datetime.now(datetime.timezone.utc)
+            ## Fetch XML forecast
+            print("\n## Fetching forecast for " + str(config["latValue"]) + \
+                  ", " + str(config["longValue"]) + " ##")
+            request = "https://forecast.weather.gov/MapClick.php?lat=" + str(config["latValue"]) + \
+                      "&lon=" + str(config["longValue"]) + "&FcstType=digitalDWML"
+            print("Loading " + request)
+            response = ET.parse(urllib.request.urlopen(request))
 
-            ## Create array to hold forecast values
-            dateArray = []
+            ## Create array to hold new Quantitative Precipitation Forecast values
+            qpf = []
 
-            ## Parse XML into array with only pretty date, epoch, and conditions forecast
-            jsonData = json.load(response)
-            for x in jsonData['forecast']['simpleforecast']['forecastday']:
-                dateArray.append([x['date']['pretty'],x['date']['epoch'],x['conditions']])
+            ## Parse XML into array with only precipitation values (in in/hr)
+            for child in response.getroot().find('.//hourly-qpf'):
+                qpf.append(float(child.text))
 
-            print("\nCurrent Forecast for current day, plus next 9 is:")
-            for x in dateArray:
-                print x[0] + ", " + x[1] + ", " + x[2]
-
-            ##Check current day for rain
-            print "\n### START Checking if raining TODAY ###"
-            if(CheckCondition(dateArray[0][2])): ## If is raining today
-                lastRain = float(dateArray[0][1]) ## Save current rain forecast as last rain globally
-                print "It will rain today. Storing current epoch as 'last rain': " + str(lastRain)
+            print("Calculating rainfall totals...")
+            if len(qpf) >= config["lookAhead"]: ## Make sure we actually gathered data
+                ## Process forecast data
+                rainForecasted = processForecast(qpf)
             else:
-                print "No rain today"
-            print "### END Checking if raining now ###\n"
+                print("Forecast too short.")
+                raise ValueError
 
-            ##Check if rain is forecast within current range
-            print "### START Checking for rain in forecast range ###"
-            for x in range(1, daysDisabled+1):
-                print "Checking " + dateArray[x][0] + " for rain conditions:"
-                if(CheckCondition(dateArray[x][2])):
-                   print("Rain has been forecast. Disabling watering")
-                   rainForecasted = True ##Set global variable outside function scope
-                   break
-                else:
-                   print("No rain found for current day. Watering may commence")
-                   rainForecasted = False ##Set global variable outside function scope
-            print "### END Checking if rain in forecast ###\n"
-
-            ## Now that we know current conditions and forecast, modify watering schedule
-            ModifyWatering()
+            ## Cache qpf table as fallback
+            oldQpf = qpf
 
             GPIO.output(11,False) ## Turn off flashing red data error light if flashing, routine successful
-            print "Checking forecast again in " + str(checkIncrement / 60) + " minute(s)"
-            time.sleep(checkIncrement)
             
         except: ## Data unavailable - either connection error, or network error
             GPIO.output(11,True) ## Turn on flashing red data error light
-            print "Error contacting weather.com. Trying again in " + str(checkIncrement / 60) + " minute(s)"
-            time.sleep(checkIncrement)  ## Reattempt connection in 1 increment
+            if len(oldQpf)>config["lookAhead"]:
+                print("Error contacting weather.gov. Using cached forecast data.")
+                del oldQpf[:1] ## Remove old first entry in qpf table, since we won't be fetching a new one
+                rainForecasted = processForecast(oldQpf)
+            else:
+                print("Error contacting weather.gov. Insufficient cached data.")
+                historicalRain = [] ## Clearing historical data, since it is now inaccurate
 
-def CheckCondition(value):
-    for x in possibleConditions:
-        if value == x:
-            print 'Rain condition found';
-            return True
+        ## Now that we know current conditions and forecast, modify watering schedule
+        ModifyWatering(rainForecasted)
 
-def ModifyWatering():
-    print "\nLast rain from forecast timestamp: " + str(lastRain)
-    print "Current Time: " + str(time.time())
-    print "Days since last rain: " + str((time.time() - lastRain)/86400 )
-    print "Seconds since last rain: " + str(time.time() - lastRain)
-    print "Days disabled in seconds: " + str(daysDisabled * 86400)
-    print "Has NOT rained within daysDisabled range: " + str(time.time() - lastRain >= daysDisabled * 86400)
+        print("Checking forecast again in " + str(int(config["checkIncrement"] / 60)) + " minute(s)")
+        time.sleep(config["checkIncrement"])
 
-    if(rainForecasted == False and time.time() - lastRain >= daysDisabled * 86400):
-        print "Hasn't rained in a while, and not expected to rain. Watering enabled."
+def processForecast(qpf):
+    global historicalRain
+    
+    ## Add current rain amount to front of historical list and trim to 7 days
+    historicalRain.insert(0,qpf[0]) 
+    del historicalRain[168:]
+
+    ## If there's not enough historical data, look ahead more
+    histLen = len(historicalRain)
+    lookAhead = config["lookAhead"]
+    if histLen < config["lookBehind"]:
+        print("Only " + str(histLen) + " hour(s) of historical data available --")
+        lookAhead = min(lookAhead + config["lookBehind"] - histLen,168)
+        print("     looking ahead " + str(lookAhead) + " hours.")
+
+    ## Total rainfall ahead and behind. First value in qpf is skipped,
+    ## as it is the current hour (and is counted in historicalRain)
+    sampledRain = qpf[1:lookAhead + 1] + \
+        historicalRain[:config["lookBehind"]]
+
+    ## Check if rainfall exceeds rate
+    rainRate = 168 * float(sum(sampledRain) / len(sampledRain))
+    if (rainRate > config["rainfallLimit"]):
+        print("Forecasted rainfall of " + str(round(rainRate,3)) + \
+            "in/wk exceeds limit of " + \
+            str(config["rainfallLimit"]) + "in/wk.")
+        rainForecasted = True
+    else:
+        print("Forecasted rainfall of " + str(round(rainRate,3)) + \
+        "in/wk is less than " + \
+        str(config["rainfallLimit"]) + "in/wk limit.")
+        rainForecasted = False
+
+    return rainForecasted
+
+def ModifyWatering(rainForecasted):
+
+    if(rainForecasted == False):
+        print("Watering enabled.")
         GPIO.output(7,False) ## Turn off relay switch, enable watering
         GPIO.output(13,True) ## Turn on green light
         GPIO.output(15,False) ## Turn off red light
     else:
+        print("Watering disabled.")
         GPIO.output(7,True) ## Turn on relay switch, disable watering
         GPIO.output(13,False) ## Turn off green light
         GPIO.output(15,True) ## Turn on red light
-        if(rainForecasted):
-            print "Rain is forecasted, or raining today. Watering Disabled"
-        else:
-            print "Rain not in forecast, but it has rained recently. Watering Disabled"
-        
+
 ## Init Forecast method
 CheckWeather()
